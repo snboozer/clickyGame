fbc58d1664dd2779156c853d21b5a1128a1ea046
Shamera <snboozer@gmail.com>
2019-10-10 19:53:05 -0400

    Updates

---
+++ precache-manifest.3f533abaaaf90d36e8c3b31b47937295.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95eec241518e82403d0935919ae051a1",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "bcbf5a7898d136af114f",
+    "url": "/clickyGame/static/css/main.256cdc3b.chunk.css"
+  },
+  {
+    "revision": "ae24bba23be4bd0d3067",
+    "url": "/clickyGame/static/js/2.ff95c248.chunk.js"
+  },
+  {
+    "revision": "bcbf5a7898d136af114f",
+    "url": "/clickyGame/static/js/main.d39fe6b2.chunk.js"
+  },
+  {
+    "revision": "60112c3cb35288f2103b",
+    "url": "/clickyGame/static/js/runtime-main.023fb769.js"
+  },
+  {
+    "revision": "105c4eaf100c09defa6772b979796fde",
+    "url": "/clickyGame/static/media/balloons.105c4eaf.jpg"
+  },
+  {
+    "revision": "bb2975996e4c1078991c6ac903d3b7ca",
+    "url": "/clickyGame/static/media/butterflies.bb297599.jpg"
+  },
+  {
+    "revision": "a2e40be146427dfb24237a906e622002",
+    "url": "/clickyGame/static/media/candy.a2e40be1.jpg"
+  },
+  {
+    "revision": "6c78409d44d39526ff6ac1477372ec98",
+    "url": "/clickyGame/static/media/flower.6c78409d.jpg"
+  },
+  {
+    "revision": "b3fa0f5f52a70123c8812eb329b43714",
+    "url": "/clickyGame/static/media/ink.b3fa0f5f.jpg"
+  },
+  {
+    "revision": "9635ceb16f2825f4e3f419bf76df25ae",
+    "url": "/clickyGame/static/media/paint.9635ceb1.jpg"
+  },
+  {
+    "revision": "fbad98f6ffd6a62c0c16bcdcd946b2c2",
+    "url": "/clickyGame/static/media/pencils.fbad98f6.jpg"
+  },
+  {
+    "revision": "325ae69af72daecb67f363caea8005d2",
+    "url": "/clickyGame/static/media/splashes.325ae69a.jpg"
+  },
+  {
+    "revision": "ce06ee98c9c9f8f1bb9413bc889419a3",
+    "url": "/clickyGame/static/media/world.ce06ee98.jpg"
+  }
+]);
