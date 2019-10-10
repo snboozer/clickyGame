8e21708db801ae5cbde10300e39b9f07a559b085
Shamera <snboozer@gmail.com>
2019-10-09 22:16:56 -0400

    updated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./style.css"
 
 class App extends React.Component {
   state = {
@@ -24,16 +24,41 @@ class App extends React.Component {
     ]
   }
 
+  handleClick = (name) => {
+    
+
+    if (this.state.clicked.includes(name)) {
+      this.setState({ losses: this.state.losses + 1, score: 0, clicked: [] })
+    }
+    else {
+      this.state.clicked.push(name)
+      const newPics = this.state.pics.sort((a, b) => .5 - Math.random())
+      this.setState({ pics: newPics, score:this.state.score+1 })
+
+      // if 
+        // (this.state.cliked(=this.state.length (this.setState({ wins: this.state.wins +1}
+
+      
+        }
+
+
+  }
+
 
 
   render() {
     return (
       <div>
         <h1>Clicky Game</h1>
+        <h2>Score: {this.state.score} </h2>
+        <h2>Wins: {this.state.wins} </h2>
+        <h2>Losses: {this.state.losses} </h2>
         <div>
           {this.state.pics.map(img => {
             return (
-              <img src={img.src}></img>
+              <img src={img.src} alt={img.name}
+                onClick={() => this.handleClick(img.name)}
+              ></img>
             )
           })}
         </div>
